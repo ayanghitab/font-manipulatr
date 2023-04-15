1e3e2c586f038b9d113ea3361f8a684e2ab9ac51
ayanghitab <128243609+ayanghitab@users.noreply.github.com>
2023-04-15 10:53:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
-function setup() {
-    video = createCapture(VIDEO);
-    video.size(550, 500);
-    
-    canvas = createCanvas(550, 550);
-    canvas.position(560,100);
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-}
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
 
-function draw() {
-    background('orange');
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log('PoseNet Is Initialized!');
+  console.log('PoseNet Is Initialized!');
 }
+
 
 function gotPoses(results)
 {
-    if(results.length > 0)
-    {
-        console.log(results);
-    }
+  if(results.length > 0)
+  {
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
+  }
+}
+
+function draw() {
+background('pink');
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+  textSize(difference);
+  fill('black');
+  text('Ayanghita', 50, 400);
 }
